2ba865077ddf5ae6f454501a2b18e408713f11ef
liavunn <liafenr@gmail.com>
2026-03-22 17:14:45 +0100

    Define Greeting struct and get_greeting function
    Added structure for country greetings and function prototype.

--- data.h
+++ data.h
@@ -1,1 +1,13 @@
+#ifndef DATA_H_
+#define DATA_H_
 
+//Holds country code and its greeting messege.
+typedef struct {
+const char* country_code;
+const char* country_greeting;
+} Greeting;
+
+//Identify the country code and return the greeting message.
+const char* get_greeting(const char* country_code);
+
+#endif
