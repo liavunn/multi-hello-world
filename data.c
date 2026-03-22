db98ca53b17d54f7d61fb019bae847d2d5d85d43
liavunn <liafenr@gmail.com>
2026-03-22 18:23:57 +0100

    Add multilingual greetings to data.c

--- data.c
+++ data.c
@@ -1,1 +1,24 @@
+#include "data.h"
+#include "string.h"
 
+static const Greeting kGreetings[]= {
+  {"zh_CN", "你好，世界"}，
+  {"zh_TW", "你好，世界"}
+  {"en_US", "Hello World"}
+  {"en_GB", "Hello World"}
+  {"es_ES", "Hola mundo"}
+  {"fr_FR", "Bonjour, le Monde"}
+  {"ja_JP", "こんにちは,世界"}
+  {"ru_RU", "Привет, мир"}
+  {"de_DE", "Hallo Welt"}
+  {"pt_BR", "Olá Mundo"}
+};
+
+static const int kNumGreetings = sizeof(kGreetings)/sizeof(kGreetings[0]);
+
+const char* get_greeting(const char* country_code) {
+for(int i = 0; i < kNumGreetings; ++i)
+  if (strcmp(country_code, kGreetings[i].country_code) == 0) {
+    return kGreetings[i].country_greeting;
+  }
+}
